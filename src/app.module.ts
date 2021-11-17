f319a6fb3712a4f0bcd39bd0ecf62b746937fdb6
MarcaCriss <54193589+MarcaCriss@users.noreply.github.com>
2021-11-17 09:55:13 -0400

    connection mysql with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USERNAME,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
+} from './config/constants';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>(DATABASE_HOST),
+        port: parseInt(config.get<string>(DATABASE_PORT), 10),
+        username: config.get<string>(DATABASE_USERNAME),
+        password: config.get<string>(DATABASE_PASSWORD),
+        database: config.get<string>(DATABASE_NAME),
+        entities: [__dirname + './**/**/*entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+        logger: 'file',
+      }),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
